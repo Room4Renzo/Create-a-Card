f4aa3cd205909ed074b7b3f79f73936fba6c76cb
Room4Renzo <lorenzo.afw@gmail.com>
2023-03-22 14:41:45 +0000

    removed small syntax error

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Carousel, { CarouselItem } from "./components/Carousel";
 
 import TextInput from "./components/TextInput";
@@ -8,55 +8,53 @@ import Navbar from "./components/Navbar";
 
 export default function App() {
 	return (
-
-<Router >
-<Navbar />
-          <Routes >
-		  <Route path="/" element={ <Carousel>
-				<CarouselItem>Item 1</CarouselItem>
-				<CarouselItem>Item 2</CarouselItem>
-				<CarouselItem>Item 3</CarouselItem>
-				<CarouselItem>Item 4</CarouselItem>
-
-
-		<div className="App">
-			<Carousel>
-				<CarouselItem>
-					<img
-						src="./assets/images/Birthday.png"
-						className="carousel-item"
-					/>
-				</CarouselItem>
-				<CarouselItem>
-					<img
-						src="./assets/images/Birthday.png"
-						className="carousel-item"
-					/>
-				</CarouselItem>
-				<CarouselItem>
-					<img
-						src="./assets/images/Birthday.png"
-						className="carousel-item"
-					/>
-				</CarouselItem>
-				<CarouselItem>
-					<img
-						src="./assets/images/Birthday.png"
-						className="carousel-item"
-					/>
-				</CarouselItem>
-
-			</Carousel>
-			<TextInput />
-		</div>
+		<Router>
+			<Navbar />
+			<Routes>
+				<Route
+					path="/"
+					element={
+						<Carousel>
+							<CarouselItem>Item 1</CarouselItem>
+							<CarouselItem>Item 2</CarouselItem>
+							<CarouselItem>Item 3</CarouselItem>
+							<CarouselItem>Item 4</CarouselItem>
+							<div className="App">
+								<Carousel>
+									<CarouselItem>
+										<img
+											src="./assets/images/Birthday.png"
+											className="carousel-item"
+										/>
+									</CarouselItem>
+									<CarouselItem>
+										<img
+											src="./assets/images/Birthday.png"
+											className="carousel-item"
+										/>
+									</CarouselItem>
+									<CarouselItem>
+										<img
+											src="./assets/images/Birthday.png"
+											className="carousel-item"
+										/>
+									</CarouselItem>
+									<CarouselItem>
+										<img
+											src="./assets/images/Birthday.png"
+											className="carousel-item"
+										/>
+									</CarouselItem>
+								</Carousel>
+								<TextInput />
+							</div>
+						</Carousel>
+					}
+				/>
+				{/* <Route path="/about" element={<About/>} />
+            <Route path="/contact" element={<Contact/>} /> */}
+				{/**/}
+			</Routes>
+		</Router>
 	);
 }
-
-			</Carousel> } />
-            {/* <Route path="/about" element={<About/>} />
-            <Route path="/contact" element={<Contact/>} /> */}
-		  {/**/}
-          </Routes>
-    </Router>
-)}
-
