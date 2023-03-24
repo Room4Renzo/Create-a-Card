3b5eb2849ccdc46a4d3cd3b5ac6ba810f1869624
Room4Renzo <lorenzo.afw@gmail.com>
2023-03-24 20:59:02 +0000

    Carousel Data now categorised and displaying correctly

--- src/App.js
+++ src/App.js
@@ -1,34 +1,40 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Carousel, { CarouselItem } from "./components/Carousel";
 import About from "./pages/About.js";
 import Contact from "./pages/Contact.js";
 import "./App.css";
+import Carousel from "./components/CarouselTest";
 import TextInput from "./components/TextInput";
 import Header from "./components/Header";
 import CardTemlate from "./components/CardTemplate";
 
 export default function App() {
-  return (
-    <Router>
-      <Header />
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <div className="app-container">
-				<div className="left-container">
-				<Carousel />
-			  	<TextInput />
-				</div>
-				
-			  	<CardTemlate />
-            </div>
-          }
-        />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
-    </Router>
-  );
+	return (
+		<Router>
+			<Header />
+			<Routes>
+				<Route
+					path="/"
+					element={
+						<div className="app-container">
+							<div className="left-container">
+								<Carousel />
+								{/* <TextInput /> */}
+							</div>
+
+							{/* <CardTemplate /> */}
+						</div>
+					}
+				/>
+				<Route
+					path="/about"
+					element={<About />}
+				/>
+				<Route
+					path="/contact"
+					element={<Contact />}
+				/>
+			</Routes>
+		</Router>
+	);
 }
--- src/components/CardTemplate.js
+++ src/components/CardTemplate.js
@@ -1,25 +1,28 @@
 import "./CardTemplate.css";
-import { useState } from 'react';
-import Deer from "../assets/images/animals/Deer.jpg";
-import Template from "../assets/images/templates/Birthday.png";
+import { useState } from "react";
+import Deer from "../assets/images/Animals/Deer.jpg";
+import Template from "../assets/images/Templates/Birthday.png";
 
-const CardTemlate = () => {
-    const [image, setImage] = useState(Template);
-    const [clicked, setClicked] = useState(false);
+const CardTemplate = () => {
+	const [image, setImage] = useState(Template);
+	const [clicked, setClicked] = useState(false);
 
-    const uploadImage = () => {
-        setImage(Deer);
-        setClicked(!clicked);
-    }
+	const uploadImage = () => {
+		setImage(Deer);
+		setClicked(!clicked);
+	};
 
-    return (
-        <div className="card-template-container">
-            <img src={Deer} onClick={uploadImage}/>
-            <div className="rectangle" >
-                { clicked ? <img src={Deer} /> : <div></div> }
-            </div>
-        </div>
-    )
-}
+	return (
+		<div className="card-template-container">
+			<img
+				src={Deer}
+				onClick={uploadImage}
+			/>
+			<div className="rectangle">
+				{clicked ? <img src={Deer} /> : <div></div>}
+			</div>
+		</div>
+	);
+};
 
-export default CardTemlate;
+export default CardTemplate;
