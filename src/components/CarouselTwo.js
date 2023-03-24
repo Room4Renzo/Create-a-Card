afa56873f4778b6dd07a596e9ae83f5e0cdf5f72
Room4Renzo <lorenzo.afw@gmail.com>
2023-03-24 11:39:28 +0000

    Completed basice version of the carousel

--- src/components/CarouselTwo.js
+++ src/components/CarouselTwo.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import "./Carousel.css";
 import { CarouselData } from "../data/CarouselData";
+import ArrowBackIosNewIcon from "@mui/icons-material/ArrowBackIosNew";
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 
 function CarouselTwo() {
 	const [activeIndex, setActiveIndex] = useState(0);
@@ -10,9 +12,28 @@ function CarouselTwo() {
 				className="inner"
 				style={{ backgroundImage: `url(${CarouselData[activeIndex].picture})` }}
 			>
-				<div className="carousel-left"></div>
+				<div
+					className="carousel-left"
+					onClick={() => {
+						if (activeIndex > 0) setActiveIndex(activeIndex - 1);
+					}}
+				>
+					<ArrowBackIosNewIcon style={{ fontSize: 30 }} />
+				</div>
 				<div className="carousel-center"></div>
-				<div className="carousel-right"></div>
+				<div
+					className="carousel-right"
+					onClick={() => {
+						if (activeIndex < CarouselData.length - 1)
+							setActiveIndex(activeIndex + 1);
+					}}
+				>
+					<ArrowForwardIosIcon
+						style={{
+							fontSize: 30,
+						}}
+					/>
+				</div>
 			</div>
 		</div>
 	);
