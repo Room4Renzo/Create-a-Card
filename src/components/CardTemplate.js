297d1238fed89577baca7404aef336230a6f4b32
Olena Pelagenko <84686704+ljenchik@users.noreply.github.com>
2023-03-25 22:39:35 +0000

    Revert "Merge branch 'main' into carousel-testing"
    This reverts commit 34af386fa10dab52c0e2f2e1001d9ef68659e369, reversing
    changes made to a9a9cc2ce837d843e27a586e1b9b33882cc8eb9e.

--- src/components/CardTemplate.js
+++ src/components/CardTemplate.js
@@ -19,23 +19,6 @@ const CardTemplate = () => {
 			<div className="rectangle"></div>
 		</div>
 	);
-const CardTemplate = ({ image, className}) => {
-  // const [image, setImage] = useState(props);
-  // const [clicked, setClicked] = useState(false);
-  // const uploadImage = () => {
-  //     setImage(Deer);
-  //     setClicked(!clicked);
-  // }
-
-  return (
-    <div className="card-template-container">
-      <img src={image} className={className}/>
-      {/* <img src={Deer} onClick={uploadImage}/> */}
-      {/* <div className="rectangle" >
-                { clicked ? <img src={Deer} /> : <div></div> }
-            </div> */}
-    </div>
-  );
 };
 
 // const CardTemplate = ({ image }) => {
