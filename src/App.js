144e16f7a63db09c0b80450130f45304cd7ad175
Room4Renzo <lorenzo.afw@gmail.com>
2023-03-25 17:21:12 +0000

    fixed some image linking issues

--- src/App.js
+++ src/App.js
@@ -9,38 +9,46 @@ import Header from "./components/Header";
 import CardTemlate from "./components/CardTemplate";
 import ResizeImage from "./components/ResizeImage";
 import DownloadButton from "./components/DownloadButton";
-import BirthdayCardTemplate from "./assets/images/templates/birthday-template1.jpeg";
+import BirthdayCardTemplate from "./assets/images/Templates/birthday-template1.jpeg";
 
 export default function App() {
-  return (
-    <Router>
-      <Header />
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <div className="app-container">
-              <div className="left-container">
-                {/* <Carousel /> */}
-                <TextInput />
-                {/* <CardTemlate image={BirthdayCardTemplate} /> */}
-              </div>
-              {/* <div className="left-container">
+	return (
+		<Router>
+			<Header />
+			<Routes>
+				<Route
+					path="/"
+					element={
+						<div className="app-container">
+							<div className="left-container">
+								{/* <Carousel /> */}
+								<TextInput />
+								{/* <CardTemlate image={BirthdayCardTemplate} /> */}
+							</div>
+							{/* <div className="left-container">
                 <Carousel />
                 <TextInput />
               </div> */}
-              {/* <ResizeImage /> */}
-              <div className="wrapper" id="download-image">
-              {/* <DownloadButton /> */}
-              {/* <CardTemlate /> */}
-              </div>
-             
-            </div>
-          }
-        />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
-    </Router>
-  );
+							{/* <ResizeImage /> */}
+							<div
+								className="wrapper"
+								id="download-image"
+							>
+								{/* <DownloadButton /> */}
+								{/* <CardTemlate /> */}
+							</div>
+						</div>
+					}
+				/>
+				<Route
+					path="/about"
+					element={<About />}
+				/>
+				<Route
+					path="/contact"
+					element={<Contact />}
+				/>
+			</Routes>
+		</Router>
+	);
 }
