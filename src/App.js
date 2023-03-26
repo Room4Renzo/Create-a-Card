c6629c748c504f10a17aa1869dc9496d8f023cf0
Room4Renzo <lorenzo.afw@gmail.com>
2023-03-26 13:29:28 +0100

    Made some corrections to syntax and naming errors preventing app from loading

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import About from "./pages/About.js";
 import Contact from "./pages/Contact.js";
@@ -6,10 +5,14 @@ import "./App.css";
 import Carousel from "./components/CarouselTest";
 import TextInput from "./components/TextInput";
 import Header from "./components/Header";
-import CardTemlate from "./components/CardTemplate";
+import CardTemplate from "./components/CardTemplate";
 import ResizeImage from "./components/ResizeImage";
 import DownloadButton from "./components/DownloadButton";
 import BirthdayCardTemplate from "./assets/images/Templates/birthday-template1.jpeg";
+import { Container } from "@mui/material";
+import CssBaseline from "@mui/material/CssBaseline";
+import Grid2 from "@mui/material/Unstable_Grid2/Grid2.js";
+import Dropdown from "./components/Dropdown.js";
 
 export default function App() {
 	return (
@@ -23,6 +26,13 @@ export default function App() {
 					rowSpacing={1}
 					columnSpacing={{ xs: 1, sm: 2, md: 3 }}
 				>
+					<Grid2
+						item
+						xs={4}
+						md={5}
+					>
+						<Dropdown />
+					</Grid2>
 					<Grid2
 						item
 						xs={4}
--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,38 +1,77 @@
 import { useState } from "react";
 import "./Dropdown.css";
 
-import BirthdayCardTemplate from "../assets/images/templates/birthday-template1.jpeg";
-import WeddingCardTemplate from "../assets/images/templates/wedding-template1.jpeg";
-import GetWellSoonCardTemplate from "../assets/images/templates/get-well-soon-template1.jpeg";
+import BirthdayCardTemplate from "../assets/images/Templates/birthday-template1.jpeg";
+import WeddingCardTemplate from "../assets/images/Templates/wedding-template1.jpeg";
+import GetWellSoonCardTemplate from "../assets/images/Templates/get-well-soon-template1.jpeg";
 
 import CardTemplate from "./CardTemplate";
 
 const options = [
-  {
-    category: "Birthday Card",
-    template: BirthdayCardTemplate,
-  },
-  {
-    category: "Wedding Card",
-    template: WeddingCardTemplate,
-  },
-  {
-    category: "Get Well Soon",
-    template: GetWellSoonCardTemplate,
-  },
+	{
+		category: "Birthday Card",
+		template: BirthdayCardTemplate,
+	},
+	{
+		category: "Wedding Card",
+		template: WeddingCardTemplate,
+	},
+	{
+		category: "Get Well Soon",
+		template: GetWellSoonCardTemplate,
+	},
 ];
 
 const Dropdown = () => {
-  const [open, setOpen] = useState(true);
-  const [cardTemplate, setCardTemplate] = useState(options[0].template);
-  const [clickedCardCategory, setClickedCardCategory] = useState(false);
+	const [open, setOpen] = useState(true);
+	const [cardTemplate, setCardTemplate] = useState(options[0].template);
+	const [clickedCardCategory, setClickedCardCategory] = useState(false);
 
-  const setTemplate = (name) => {
-    console.log(name.category);
-    console.log(name.template);
-    setCardTemplate(name.template);
-    setClickedCardCategory(true);
-  };
+	const setTemplate = (name) => {
+		console.log(name.category);
+		console.log(name.template);
+		setCardTemplate(name.template);
+		setClickedCardCategory(true);
+	};
+
+	<ul
+		hidden={open}
+		onClick={() => setOpen(!open)}
+		className="menu-list"
+	>
+		{options.map((option) => (
+			<li
+				className="menu-item"
+				key={option.category}
+				onClick={() => setTemplate(option)}
+			>
+				{option.category}
+			</li>
+		))}
+	</ul>;
+	{
+		clickedCardCategory ? (
+			<CardTemplate
+				image={cardTemplate}
+				className="rectangle"
+			/>
+		) : (
+			<CardTemplate
+				image={BirthdayCardTemplate}
+				className="rectangle"
+			/>
+		);
+	}
+
+	return (
+		<div>
+			<div className="dropdown-menu">
+				<button
+					onClick={() => setOpen(!open)}
+					className="menu-button"
+				>
+					Category
+				</button>
 
 				<ul
 					hidden={open}
@@ -55,42 +94,17 @@ const Dropdown = () => {
 						className="rectangle"
 					/>
 				) : (
-					<CardTemplate
-						image={BirthdayCardTemplate}
-						className="rectangle"
-					/>
+					<div></div>
 				)}
 			</div>
 		</div>
 	);
-  return (
-    <div>
-      <div className="dropdown-menu">
-        <button onClick={() => setOpen(!open)} className="menu-button">
-          Category
-        </button>
-
-        <ul hidden={open} onClick={() => setOpen(!open)} className="menu-list">
-          {options.map((option) => (
-            <li
-              className="menu-item"
-              key={option.category}
-              onClick={() => setTemplate(option)}
-            >
-              {option.category}
-            </li>
-          ))}
-        </ul>
-        {clickedCardCategory ? <CardTemplate image={cardTemplate} className="rectangle"/> : <div></div> }
-      </div>
-    </div>
-  );
 };
 
 export default Dropdown;
 
 {
-  /* <div className="appear">
+	/* <div className="appear">
 {clickedCardCategory ? <CardTemplate image={cardTemplate} /> : <CardTemplate image={"../assets/images/templates/birthday-template2.jpeg"} />}
 {/* <CardTemplate image={"../assets/images/templates/birthday-template1.jpeg"} /> */
 }
