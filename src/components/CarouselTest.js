a9a219f4146cbba71e5e52928989376602a957a6
Room4Renzo <lorenzo.afw@gmail.com>
2023-03-26 00:00:53 +0000

    reverted changes to working state

--- src/components/CarouselTest.js
+++ src/components/CarouselTest.js
@@ -3,60 +3,22 @@ import "./CarouselTest.css";
 import CarouselData from "../data/CarouselData";
 import ArrowBackIosNewIcon from "@mui/icons-material/ArrowBackIosNew";
 import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
-import CardTemplate from "./CardTemplate";
-import { Transform } from "@mui/icons-material";
-import { Fragment } from "react";
 
 function Carousel() {
 	const [activeIndex, setActiveIndex] = useState(0);
 	const [image, setImage] = useState(CarouselData[activeIndex].picture);
 	const [paused, setPaused] = useState(false);
-	// const [category, setCategory] = useState(CarouselData[activeIndex].category);
 	const currentImage = CarouselData[activeIndex].picture;
 	const [clicked, setClicked] = useState(false);
 	const activeImage = CarouselData[activeIndex];
 
-	const updateIndex = () => {
-		if (activeIndex < 0) {
-			activeIndex = CarouselData.length - 1;
-		} else if (activeIndex >= CarouselData.length) {
-			activeIndex = 0;
-		}
-		setActiveIndex(activeIndex);
-	};
-
-	const uploadImage = () => {
-		setImage(currentImage);
-		setClicked(!clicked);
-	};
-
-	// const transitionImage = () => {
-	// 	const visibleImage = document.querySelector(".inner");
-	// 	visibleImage.style.transform = `translateX(-${activeIndex * 35}%)`;
-	// };
-
-	useEffect(() => {
-		const interval = setInterval(() => {
-			if (!paused) {
-				updateIndex(activeIndex + 1);
-			}
-		}, 1500);
-
-		return () => {
-			if (interval) {
-				clearInterval(interval);
-			}
-		};
-	});
-
 	return (
 		<div className="carousel">
-			{" "}
 			<div className="outer">
 				<div
 					className="inner"
 					style={{
-						backgroundImage: `url(${CarouselData[activeIndex].picture})`,
+						backgroundImage: `url(${currentImage})`,
 					}}
 				>
 					<div
@@ -69,9 +31,6 @@ function Carousel() {
 					</div>
 					<div
 						className="carousel-center"
-						onClick={() => {
-							uploadImage();
-						}}
 					></div>
 					<div
 						className="carousel-right"
