c1ea047c2536ae2913af940d00b6056fbe8d7755
Olena Pelagenko <84686704+ljenchik@users.noreply.github.com>
2023-03-29 21:17:32 +0100

    Restructured and added screenshot

--- src/App.js
+++ src/App.js
@@ -1,81 +1,21 @@
-import ResizableElements from "./components/ResizableElements.jsx";
-import { Resizable } from "re-resizable";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import { useState } from "react";
-import "./App.css";
-import Carousel from "./components/CarouselTest";
-import Header from "./components/Header";
-import CardTemplate from "./components/CardTemplate";
-import ResizeImage from "./components/ResizeImage";
-import DownloadButton from "./components/DownloadButton";
-import "bulma/css/bulma.css";
-import TemplateCarousel from "./components/TemplateCarousel.js";
-import FontDropdown from "./components/FontDropdown";
-import TextWrapper from "./components/ResizableElements.jsx";
-import CategoryDropdown from "./components/CategoryDropdown.jsx";
-import { identifier } from "@babel/types";
-import TemplateData from "./data/TemplateData";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import MyCard from "./components/MyCard";
+
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 export default function App() {
-	const [name, setName] = useState("");
-	const [message, setMessage] = useState("");
 
-	const handleChange = (event) => {
-		console.log(event.target.value);
-		setName(event.target.value);
-		setMessage(event.target.value);
-	};
-
-	const [image, setImageUrl] = useState();
-	const [template, setTemplateUrl] = useState();
 
 	return (
-		<Router>
-			<Header />
-			<div className="container is-fluid">
-				<div className="columns is-vcentered">
-					<div className="column is-5">
-						{/* <h1 className="title is-size-2">Create your own card</h1> */}
-						<TemplateCarousel
-							onClickTemplate={(template) => setTemplateUrl(template)}
-						/>
-						<Carousel onClickImage={(image) => setImageUrl(image)} />
-					</div>
-
-					<div className="column is-2">
-						<div className="card is-shadowless">
-							<div className="content">
-								<h2 className="is-size-2">← Pick an image here</h2>
-							</div>
-						</div>
-
-						<div className="card is is-shadowless">
-							<div className="content">
-								<h2 className="is-size-2">Design your card here →</h2>
-							</div>
-						</div>
-
-						<div className="card is-shadowless">
-							<div className="content">
-								<h2 className="is-size-2">Enter Text Here ↓</h2>
-							</div>
-							{/* <TextInput
-								name="name"
-								handleChange={handleChange}
-							/> */}
-
-							{/* <ResizableElements /> */}
-
-						</div>
-					</div>
-				</div>
-			</div>
-			<div className="column is-6">
-				<CardTemplate
-					templateUrl={template}
-				/>
-				<ResizeImage image={image} />
-			</div>
+		<Router >
+			<Navbar />
+			  <Routes >
+				<Route path="/" element={<Home/>} />
+				<Route path="/my-card" element={<MyCard/>} />
+			  </Routes>
 		</Router>
-	);
+	  );
 }
+
---
+++ src/components/MyCard.js
@@ -0,0 +1,5 @@
+const MyCard = () => {
+    return <h1>My card</h1>
+}
+
+export default MyCard;
