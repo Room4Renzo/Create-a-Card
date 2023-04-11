a0a845e744b0d3a191807756780020ecf75f91b3
Olena Pelagenko <84686704+ljenchik@users.noreply.github.com>
2023-04-11 19:21:11 +0100

    Added instructions, github links and a bit of styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Navbar from "./components/Navbar/Navbar";
 import Home from "./components/Home/Home";
+import Footer from "./components/Footer/Footer";
 
 export default function App() {
 	return (
 		<div>
 			<Navbar />
 			<Home />
+			<Footer />
 		</div>
 	);
 }
